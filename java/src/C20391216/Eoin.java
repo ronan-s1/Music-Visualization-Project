c92469d49a309a102652cf596de9f199f9e8385a
Eoin <eoinot128@gmail.com>
2022-04-08 22:13:05 +0100

    wabd

--- java/src/C20391216/Eoin.java
+++ java/src/C20391216/Eoin.java
@@ -2,7 +2,7 @@ package C20391216;
 
 import ie.tudublin.Visual;
 
-public class Eoin
+public class Eoin extends Visual
 {
     
     float[] lerpedBuffer;
@@ -10,7 +10,7 @@ public class Eoin
     float y = 0;
     float smoothedY = 0;
     float smoothedAmplitude = 0;
-    
+
     float halfH = height / 2;
     float average = 0;
     float average2 = 0;
@@ -37,28 +37,34 @@ public class Eoin
     }
 
     average2= sum / (float) ab.size();
+}
+
 
     public void render(Heathens h)
     {
+        
+        h.background(0);
+
         for(int i = 0 ; i < ab.size() ; i ++)
         {
             
             float c = map(i, 0, ab.size(), 0, 255);
-            stroke(c, 255, 255);
+            h.stroke(c, 255, 255);
             float f1 = lerpedBuffer[i] * halfH * 1.7f;
-            rect(i, halfH*2.0f + f1, i, halfH - f1);    
+            h.rect(i, halfH*2.0f + f1, i, halfH - f1);    
             
             float f2 = lerpedBuffer[i] * halfH * 1.7f;
-            rect(i,0 - f2, i, 0 + f2); 
+            h.rect(i,0 - f2, i, 0 + f2); 
 
             float f3 = lerpedBuffer2[i] * halfH * 1.7f;
-            rect(0+f3,i, 0-f3, i); 
+            h.rect(0+f3,i, 0-f3, i); 
 
             float f4 = lerpedBuffer2[i] * halfH * 1.7f;
-            rect(height-f4,i, width + f4, i); 
+            h.rect(height-f4,i, width + f4, i); 
 
         }
 
         
+    }
 }
 
