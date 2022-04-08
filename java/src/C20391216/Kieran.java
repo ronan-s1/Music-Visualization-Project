3b00792e5d723cd517a1cd4ce70e008feb292271
C20483514 Kieran Retardo Silada <c20483514@mytudublin.ie>
2022-04-08 13:24:27 +0100

    class constructor

--- java/src/C20391216/Kieran.java
+++ java/src/C20391216/Kieran.java
@@ -4,14 +4,8 @@ import ie.tudublin.Visual;
 
 public class Kieran
 {
-    int mode = 1;
-    boolean paused = false;
-
     public void render(Heathens h)
     {
-        h.background(0);
-        h.fill(255);
-        h.textSize(16);
-        h.text("TEST", 400, 400);
+        
     }
 }
