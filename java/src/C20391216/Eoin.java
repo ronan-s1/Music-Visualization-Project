ef11d2de2b509d73d1f1ba868835c4f74608f012
Eoin <eoinot128@gmail.com>
2022-04-08 19:33:13 +0100

    doesnt work

--- java/src/C20391216/Eoin.java
+++ java/src/C20391216/Eoin.java
@@ -4,50 +4,61 @@ import ie.tudublin.Visual;
 
 public class Eoin
 {
-    boolean twocubes = false;
-    float smoothedBoxSize = 0;
+    
+    float[] lerpedBuffer;
+    float[] lerpedBuffer2;
+    float y = 0;
+    float smoothedY = 0;
+    float smoothedAmplitude = 0;
+    
+    float halfH = height / 2;
+    float average = 0;
+    float average2 = 0;
+    float sum = 0;
+
+    smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
+    
+    float cx = width / 2;
+    float cy = height / 2;
+
+    for(int i = 0 ; i < ab.size() ; i ++)
+    {
+        sum += abs(ab.get(i));
+        lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
+    }
+    average= sum / (float) ab.size();
+
+    sum = 0;
+
+    for(int i = 0 ; i < ab.size() ; i ++)
+    {
+        sum += abs(ab.get(i));
+        lerpedBuffer2[i] = lerp(lerpedBuffer2[i], ab.get(i), 0.05f);
+    }
+
+    average2= sum / (float) ab.size();
+
     public void render(Heathens h)
     {
-        h.calculateAverageAmplitude();
-        h.background(0);
-        h.noFill();
-        h.lights();
-        h.stroke(Visual.map(h.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-        h.camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
-        h.translate(0, 0, -250);
-               
-        float boxSize = 50 + (h.getAmplitude() * 300);//map(average, 0, 1, 100, 400); 
-        smoothedBoxSize = Visual.lerp(smoothedBoxSize, boxSize, 0.2f);        
-        if (twocubes)
+        for(int i = 0 ; i < ab.size() ; i ++)
         {
-            h.pushMatrix();
-            h.translate(-200, 0, 0);
-           h.rotateY(angle);
-            h.rotateX(angle);
-            h.box(smoothedBoxSize);
-            //strokeWeight(1);
-            //sphere(smoothedBoxSize);
-            h.popMatrix();
-            h.pushMatrix();
-            h.translate(200, 0, 0);
-            h.rotateY(angle);
-            h.rotateX(angle);
-            h.strokeWeight(2); 
-            h.box(smoothedBoxSize);
-            h.popMatrix();
-        }
-        else
-        {
-            h.rotateY(angle * 5);
-            h.rotateX(angle * 10);
-            //strokeWeight(1);
-            //sphere(smoothedBoxSize/ 2);            
-            h.strokeWeight(5);
             
-            h.box(smoothedBoxSize);
+            float c = map(i, 0, ab.size(), 0, 255);
+            stroke(c, 255, 255);
+            float f1 = lerpedBuffer[i] * halfH * 1.7f;
+            rect(i, halfH*2.0f + f1, i, halfH - f1);    
+            
+            float f2 = lerpedBuffer[i] * halfH * 1.7f;
+            rect(i,0 - f2, i, 0 + f2); 
+
+            float f3 = lerpedBuffer2[i] * halfH * 1.7f;
+            rect(0+f3,i, 0-f3, i); 
+
+            float f4 = lerpedBuffer2[i] * halfH * 1.7f;
+            rect(height-f4,i, width + f4, i); 
+
         }
-        angle += 0.01f;
-    }
-    float angle = 0;
+
+        
 }
 
--- java/src/C20391216/Heathens.java
+++ java/src/C20391216/Heathens.java
@@ -158,7 +158,7 @@ public class Heathens extends Visual {
 
             
 
-
+            Eoin.render(this);
 
             break;
           }
