1058e53e6dcebf6a79134a81b473f2016f248566
Ronan Singpurwala <singpurwala13@gmail.com>
2022-04-13 15:49:32 +0100

    border

--- java/src/C20391216/Ronan.java
+++ java/src/C20391216/Ronan.java
@@ -4,10 +4,11 @@ import ie.tudublin.Visual;
 
 public class Ronan extends Visual
 {
-    float theta = 0.0f;
+    float theta = 0;
     int mode = 1;
     float angle = 0;
     boolean paused = false;
+
 
     public void settings()
     {
@@ -15,6 +16,7 @@ public class Ronan extends Visual
         println("CWD: " + System.getProperty("user.dir"));
         //fullScreen(P3D, SPAN);
     }
+
 
     public void keyPressed()
     {
@@ -74,7 +76,7 @@ public class Ronan extends Visual
 
     public void setup()
     {
-        // colorMode(HSB);
+        colorMode(HSB);
         // noCursor();
         
         setFrameSize(256);
@@ -85,10 +87,11 @@ public class Ronan extends Visual
         hint(ENABLE_DEPTH_SORT); // option that allows transparency
     }
 
+
     void drawCube(float cubeSpeed)
     {
         calculateAverageAmplitude();
-        stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        stroke(map(getSmoothedAmplitude(), 0, 0.8f, 0, 255), 255, 255);
         strokeWeight(5);
 
         pushMatrix();
@@ -98,13 +101,26 @@ public class Ronan extends Visual
         rotateX(angle);
         rotateZ(angle);       
 
-        float boxSize = 40 + (200 * getSmoothedAmplitude()); 
+        float boxSize = 50 + (125 * getSmoothedAmplitude());
         box(boxSize);
 
         popMatrix();
 
-        angle += cubeSpeed / 3;
+        angle += cubeSpeed * 0.4;
     }
+
+
+    void drawBorder(float smoothedAmplitude, float colour)
+    {
+        float border = map(smoothedAmplitude, 0, 0.2f, 3, 100);
+        fill(colour, 255, 255);
+        stroke(colour, 255, 255);
+        rect(0, 0, width, border); // Top
+        rect(width - border, 0, border, height); // Right
+        rect(0, height - border, width, border); // Bottom
+        rect(0, 0, border, height); // Left
+    }
+
 
     void drawCircle(float size)
     {
@@ -113,12 +129,14 @@ public class Ronan extends Visual
         circle(0, 0, size + 30);
         triangle(0, 0, 0, 0, 0, 0);
     }
+
   
     void drawPyramid(float t, float colour)
     { 
         // theres 3 vertexs for the 3 the 3 triangles on the prism
         // T is the size of the shape
         beginShape(TRIANGLES);
+        stroke(colour, 255, 255);
         
         // colour changes depending on the amplitude
         fill(colour, 255, 255, 150);
@@ -147,28 +165,30 @@ public class Ronan extends Visual
 
     public void draw()
     {
-        colorMode(HSB);
         background(0);
-
         float smoothedAmplitude = 0;
         float size = 0;
         float c = 0;
+        float c2 = 0;
         float speed = 0;
-
+       
         smoothedAmplitude = getSmoothedAmplitude() / 8;
 
-        speed = smoothedAmplitude * 1.5f;
+        size = map(smoothedAmplitude, 0, 0.1f, 10, 50);
+        c = map(smoothedAmplitude, 0, 0.07f, 0, 255);
+        c2 = map(smoothedAmplitude, 0, 0.055f, 0, 255);
+
+        speed = smoothedAmplitude * 1.6f;
         theta += speed;
+
+        drawBorder(smoothedAmplitude, c);
 
         translate(width/2, height/2, 0);
         rotateX(theta);
         rotateY(theta);
 
-        size = map(smoothedAmplitude, 0, 0.1f, 10, 45);
-        c = map(smoothedAmplitude, 0, 0.08f, 0, 255) / 2;
-
-        drawPyramid(size * 2, c * 2);
-
+        drawPyramid(size * 2.4f, c2);
+        
         translate(150, 150);
         drawPyramid(size, c);
         drawCircle(size);
@@ -184,7 +204,7 @@ public class Ronan extends Visual
         translate(-300, 300);
         drawPyramid(size, c);
         drawCircle(size);
-
+ 
         drawCube(speed);
     }
 }
