5239ea1554e5f8bcc82856c18320988b4c134f3f
Ronan <85257187+ronan-s1@users.noreply.github.com>
2022-04-25 22:24:59 +0100

    fixed indentation and shi

--- java/src/C20391216/Heathens.java
+++ java/src/C20391216/Heathens.java
@@ -17,8 +17,8 @@ public class Heathens extends Visual {
     {
         if (key >= '0' && key <= '4')
         {
-			mode = key - '0';
-		}
+		mode = key - '0';
+	}
 
         switch(key)
         {
@@ -110,7 +110,6 @@ public class Heathens extends Visual {
 
             case 4: //Ronan SECTION 4
             {
-            
                 Ronan.render(this);
                 break;
             }
